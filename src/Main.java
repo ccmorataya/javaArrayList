3efa87116e1893542e58e4c89e8578caefe1d921
Cristian Morataya <cris.morataya@gmail.com>
2016-10-11 00:06:25 -0600

    Add the menu file, cycle the input until it gets a zero

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 /**
  * Created by kali on 10/10/16.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("here i am");
+        Scanner read;
+        read = new Scanner(System.in);
+        String number = "1";
+
+        while (Integer.parseInt(number) != 0){
+            number = read.nextLine();
+        }
+        Menu.reader( Integer.parseInt(number));
     }
 }
---
+++ src/Menu.java
@@ -0,0 +1,8 @@
+/**
+ * Created by kali on 10/10/16.
+ */
+public class Menu {
+    public static void reader(int number){
+        System.out.println("el número es: " + number);
+    }
+}
