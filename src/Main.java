57e92f87ee79c1ee2e2e09d5436ff6c69ebac1ad
Cristian Morataya <cris.morataya@gmail.com>
2016-10-14 19:40:17 -0600

    Implement the three methods to iterate an ArrayList, complete the exercise

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,39 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Iterator;
 /**
  * Created by kali on 10/10/16.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Menu.reader());
+        //Declare assign the variable of type ArrayList<Integer>
+        ArrayList<Integer> variable = Menu.reader();
+        int suma = 0;
+        double promedio = 0.00;
+
+        //Loop trough the ArrayList with the for
+        for (int i =0; i< variable.size(); i ++){
+            suma += variable.get(i);
+        }
+        promedio = suma / variable.size();
+        System.out.println("La suma es: " + suma + " y el promedio es: " + promedio + ".");
+        suma = 0;
+        promedio = 0.00;
+
+        //Loop using for each
+        for(Integer s: variable){
+            suma += s;
+        }
+        promedio = suma / variable.size();
+        System.out.println("La suma es: " + suma + " y el promedio es: " + promedio + ".");
+        suma = 0;
+        promedio = 0.00;
+
+        //Loop using iterator
+        Iterator it = variable.iterator();
+        while(it.hasNext()){
+            suma += (int) it.next();
+        }
+        promedio = suma / variable.size();
+        System.out.println("La suma es: " + suma + " y el promedio es: " + promedio + ".");
     }
 }
